134c117784a65651913e3ab2040f6940029c2eb1
emmamichael <emmamichael65@gmail.com>
2020-12-01 10:48:10 +0000

    ocrandsummarize webapp

--- ocrandsummarize/settings.py
+++ ocrandsummarize/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'hqeviktb$%&x_7ahcb-*km&o+!m)@3jp+tm@j-fk-6zog6j1p*'
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
