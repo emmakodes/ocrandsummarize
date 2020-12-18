ee5d0c73a858b276d616e0b2fcb62e18657b41a5
Emmanuel Michael O <34986076+emmamichael@users.noreply.github.com>
2020-12-18 19:06:21 +0000

    Update settings.py

--- ocrandsummarize/settings.py
+++ ocrandsummarize/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'hqeviktb$%&x_7ahcb-*km&o+!m)@3jp+tm@j-fk-6zog6j1p*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emmamichael.pythonanywhere.com']
 
 # Application definition
 
